cd2d4706f273478d460c38c528d0dce8ed1c5335
Leonardo Mazoco <115808428+leomazoco@users.noreply.github.com>
2023-03-09 12:53:16 -0300

    article js

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,55 @@
 const alert = document.querySelector('.alerta');
 const form = document.querySelector('.lista-form');
 const tarefa = document.getElementById('tarefas');
-const adicionar = document.querySelector('submit-btn')
-//OPÇÃO EDITAR
+const adicionar = document.querySelector('submit-btn');
+const container = document.querySelector('container-tarefas');
+const lista = document.querySelector('lista-tarefas');
+const limparBtn = document.querySelector('clear-btn');
 
+//OPÇÃO EDITAR
+let editElement;
+let editFlag = false;
+let editID = "";
+
+form.addEventListener('submit',addItem)
 //FUNÇÕES
+function addItem(e){
+    e.preventDefault();
+    //console.log(tarefa.value);
+    const value = tarefa.value;
+    const id = new Date().getTime().toString();
+    
+    if(value && !editFlag){
+        const element = document.createElement('article');
+        //add class
+        element.classList.add('tarefa-item');
+        //add ID
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+
+        //Texto ao article
+
+        element.innerHTML = ``
+    
+    } else if(value && editFlag){
+        console.log('editando')
+    
+    } else{
+        displayAlert('Adicione uma tarefa', 'danger')
+    }
+}
+
+//ALERTA DISPLAY
+function displayAlert(text,action){
+    alert.textContent = text;
+    alert.classList.add(`alerta-${action}`);
+
+    setTimeout(function(){
+        alert.textContent = '';
+        alert.classList.remove(`alerta-${action}`)
+    }, 1500)
+}
 
 //LOCAL STORAGE
 
