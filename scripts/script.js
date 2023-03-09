ebe1540bd772df6bb7b452e1468ebe45ec2a2059
Leonardo Mazoco <115808428+leomazoco@users.noreply.github.com>
2023-03-09 11:43:28 -0300

    update

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+//SELEÇAO DE ITEM
+const alert = document.querySelector('.alerta');
+const form = document.querySelector('.lista-form');
+const tarefa = document.getElementById('tarefas');
+const adicionar = document.querySelector('submit-btn')
+//OPÇÃO EDITAR
+
+//FUNÇÕES
+
+//LOCAL STORAGE
+
+//CONFIGURAÇÃO
