ca3b852868b9f668a13dc971de8425c3b75da3bb
Leonardo Mazoco <115808428+leomazoco@users.noreply.github.com>
2023-03-09 14:09:56 -0300

    adicionado editar

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,12 @@ let editElement;
 let editFlag = false;
 let editID = "";
 
+//adicionar item
 form.addEventListener('submit',addItem)
+
+//DELETAR ITENS
+limparBtn.addEventListener('click',limparLista)
+
 //FUNÇÕES
 function addItem(e){
     e.preventDefault();
@@ -40,10 +45,14 @@ function addItem(e){
                     <i class="material-icons">delete_outline</i>
                 </button>
             </div>`;
+            const deleteBtn = element.querySelector('.delete-btn');
+            const editBtn = element.querySelector('.edit-btn');
+            deleteBtn.addEventListener('click',apagarItem);
+            editBtn.addEventListener('click',editarItem);
             //APPEND CHILD
             lista.appendChild(element);
             //DISPLAY ALERT
-            displayAlert('Item adicionado a lista', 'success');
+            displayAlert('Tarefa adicionada a lista', 'success');
             container.classList.add('mostrar-container');
 
             //ADICIONANDO AO LOCAL STORAGE
@@ -52,8 +61,11 @@ function addItem(e){
             setBackToDefault();
     
     } else if(value && editFlag){
-        console.log('editando')
-    
+        editElement.innerHTML = value;
+        displayAlert('Tarefa editada', 'success');
+        //edit local storage
+        editLocalStorage(editID,value);
+        setBackToDefault();
     } else{
         displayAlert('Adicione uma tarefa', 'danger')
     }
@@ -70,14 +82,62 @@ function displayAlert(text,action){
     }, 1500)
 }
 
+//FUNÇÃO APAGAR
+function limparLista(){
+    const itens = document.querySelectorAll('.tarefa-item');
+
+    if(itens.length > 0){
+        itens.forEach(function(item){
+            lista.removeChild(item);
+        });
+    }
+    container.classList.remove("mostrar-container");
+    displayAlert('Tarefas apagadas', 'danger');
+    setBackToDefault();
+    //localStorage.removeItem('lista');
+}
+
+//FUNÇÃO EDITAR
+function editarItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+
+    tarefa.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    adicionar.textContent = "editar"
+}
+
+//FUNÇÃO APAGAR
+function apagarItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    lista.removeChild(element);
+
+    if(lista.children.length === 0){
+        container.classList.remove('mostrar-container');
+    }
+    displayAlert('Tarefa apagada', 'danger');
+    setBackToDefault();
+
+    //APAGANDO DO BANCO DE DADOS
+    //removeFromLocalStorage(id);
+}
+
 //APAGANDO O PLACEHOLDER
 function setBackToDefault(){
-    
+    tarefa.value = "";
+    editFlag = false;
+    editID = "";
+    adicionar.textContent = "adicionar";
 }
 
 //LOCAL STORAGE
 function addToLocalStorage(id, value){
     console.log('adicionado ao banco de dados');
 }
+
+function removeFromLocalStorage(id){}
+function editLocalStorage (id,value){};
 
 //CONFIGURAÇÃO
